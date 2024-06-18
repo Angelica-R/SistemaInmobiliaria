dab130d18e390d587ab8380e49aa9f0dc8022724
rodrigo_lira <rodliraa@upt.pe>
2024-06-18 17:23:23 -0500

    Cliente Login

---
+++ Sistema_Inmobiliaria/Controllers/ClienteController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_Inmobiliaria.Models;
+
+namespace Sistema_Inmobiliaria.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private Model_Sistema db = new Model_Sistema();
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string email, string password)
+        {
+            using (var db = new Model_Sistema())
+            {
+                var usuario = db.clientes.FirstOrDefault(u => u.cliEmail == email && u.cliClave == password);
+                if (usuario != null)
+                {
+                    Session["UsuarioID"] = usuario.codcliente;
+                    return RedirectToAction("Contactanos", "Contactanos");
+                }
+                else
+                {
+                    ViewBag.ErrorMessage = "Credenciales incorrectas. Inténtalo de nuevo.";
+                    return View();
+                }
+            }
+        }
+    }
+}
