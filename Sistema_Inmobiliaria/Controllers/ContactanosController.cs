0c1abe92d9249fe693df255ac1e93f5e972b2348
rodrigo_lira <rodliraa@upt.pe>
2024-06-18 17:35:15 -0500

    Contactanos SinSesion

--- Sistema_Inmobiliaria/Controllers/ContactanosController.cs
+++ Sistema_Inmobiliaria/Controllers/ContactanosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sistema_Inmobiliaria.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,28 @@ namespace Sistema_Inmobiliaria.Controllers
 {
     public class ContactanosController : Controller
     {
-        // GET: Contactanos
-        public ActionResult Index()
+        private Model_Sistema db = new Model_Sistema();
+
+        public ActionResult Contactanos()
         {
-            return View();
+            if (Session["UsuarioID"] == null)
+            {
+                return View("SinSesion");
+            }
+            else
+            {
+                int clienteID = (int)Session["UsuarioID"];
+                var cliente = db.clientes.Find(clienteID);
+
+                var model = new citas
+                {
+                    codcliente = clienteID,
+                    clientes = cliente
+                };
+
+                ViewBag.Locales = new SelectList(db.locales, "codlocal", "localNombre");
+                return View("ConSesion", model);
+            }
         }
     }
 }
