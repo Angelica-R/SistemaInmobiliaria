22afee9eabe4b8baae93f850b4e25bbf1168f806
Angelica-R <angromeror@upt.pe>
2024-07-04 16:05:32 -0500

    Tabla-Guias-Citasv1

---
+++ Sistema_Inmobiliaria/Controllers/CitasController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_Inmobiliaria.Models;
+
+namespace Sistema_Inmobiliaria.Controllers
+{
+    public class CitasController : Controller
+    {
+        private Model_Sistema db = new Model_Sistema();
+
+        // GET: Citas
+        public async Task<ActionResult> Index(/*int? id*/)
+        {
+            var citas = await db.citas.Include(c => c.clientes).Include(c => c.locales).ToListAsync();
+            return View(citas);
+
+            //ViewBag.Clientes = await db.clientes.ToListAsync();
+            //ViewBag.Locales = await db.locales.ToListAsync();
+            //ViewBag.Citas = await db.citas.Include(c => c.clientes).Include(c => c.locales).ToListAsync();
+            //ViewBag.Cita = id == null ? new citas() : await db.citas.FindAsync(id);
+            //return View();
+        }
+
+        // POST: Citas/Guardar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Guardar([Bind(Include = "codcita,codcliente,codlocal,fecha,hora")] citas cita)
+        {
+            if (ModelState.IsValid)
+            {
+                if (cita.codcita == 0)
+                {
+                    db.citas.Add(cita);
+                }
+                else
+                {
+                    db.Entry(cita).State = EntityState.Modified;
+                }
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Clientes = await db.clientes.ToListAsync();
+            ViewBag.Locales = await db.locales.ToListAsync();
+            ViewBag.Citas = await db.citas.Include(c => c.clientes).Include(c => c.locales).ToListAsync();
+            ViewBag.Cita = cita;
+            return View("Index");
+        }
+
+
+        // POST: Citas/Eliminar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Eliminar(int id)
+        {
+            citas cita = await db.citas.FindAsync(id);
+            if (cita != null)
+            {
+                db.citas.Remove(cita);
+                await db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Sistema_Inmobiliaria/Controllers/GuiasProgramadasController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Sistema_Inmobiliaria.Models;
+
+namespace Sistema_Inmobiliaria.Controllers
+{
+    public class GuiasProgramadasController : Controller
+    {
+        private Model_Sistema db = new Model_Sistema();
+
+        // GET: GuiasProgramadas
+        public async Task<ActionResult> Index()
+        {
+            var guias = await db.guias_programadas.Include(g => g.inmuebles).ToListAsync();
+            return View(guias);
+        }
+
+        // POST: GuiasProgramadas/Guardar
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Guardar([Bind(Include = "idguia,nombre,telefono,inmueble,correo,mensaje")] guias_programadas guia)
+        {
+            if (ModelState.IsValid)
+            {
+                if (guia.idguia == 0)
+                {
+                    db.guias_programadas.Add(guia);
+                }
+                else
+                {
+                    db.Entry(guia).State = EntityState.Modified;
+                }
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Inmuebles = new SelectList(db.inmuebles, "id", "nombre", guia.inmueble);
+            return View("Crear", guia);
+        }
+
+        // POST: GuiasProgramadas/Eliminar/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Eliminar(int id)
+        {
+            guias_programadas guia = await db.guias_programadas.FindAsync(id);
+            if (guia != null)
+            {
+                db.guias_programadas.Remove(guia);
+                await db.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
